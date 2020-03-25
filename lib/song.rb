a5dd863f8702d53f9baf68c371504efb1f0fc55a
sheriefCloud <sherief@farid.cloud>
2020-03-24 17:54:49 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -33,17 +33,9 @@ class Song
         @@all << self
     end
     def self.find_by_name(song)
-        r=@@all.each {|s|s.name == song}
-        # r_song = nil
-        # @@all.each do |s| 
-            
-        #     binding.pry
-        #     if s.name == song
-        #         r_song = s
-        #     end
-
-        # end
-        # r_song
+        r_song = nil
+        @@all.each {|s| r_song = s if s.name == song}
+        r_song
     end
 
     def self.create(song)
