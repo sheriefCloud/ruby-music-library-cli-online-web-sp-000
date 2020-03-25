251b878241878e778d05091003b718d718687eba
sheriefCloud <sherief@farid.cloud>
2020-03-24 17:51:37 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -32,6 +32,19 @@ class Song
     def save
         @@all << self
     end
+    def self.find_by_name(song)
+        r=@@all.each {|s|s.name == song}
+        # r_song = nil
+        # @@all.each do |s| 
+            
+        #     binding.pry
+        #     if s.name == song
+        #         r_song = s
+        #     end
+
+        # end
+        # r_song
+    end
 
     def self.create(song)
         new_song = Song.new(song)
